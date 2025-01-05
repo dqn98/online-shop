9e650e8f4534b02c57ed2a05288ac69f60daa0eb
Microservice-Shop <namqd98@gmail.com>
2025-01-05 22:54:33 +0700

    add configure for grpc services - basket project

--- src/Services/Basket.API/Extensions/ServiceExtensions.cs
+++ src/Services/Basket.API/Extensions/ServiceExtensions.cs
@@ -1,9 +1,11 @@
-﻿using Basket.API.Repositories;
+﻿using Basket.API.GrpcServices;
+using Basket.API.Repositories;
 using Basket.API.Repositories.Interfaces;
 using Contracts.Common.Interfaces;
 using EventBus.Messages.IntegrationEvents.Events;
 using Infrastructure.Common;
 using Infrastructure.Extensions;
+using Inventory.Grpc.Protos;
 using MassTransit;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Shared.Configurations;
@@ -21,12 +23,17 @@ public static class ServiceExtensions
         
         var cacheSettings = configuration.GetSection(nameof(CacheSettings))
             .Get<CacheSettings>();
+                
+        var grpcSettings = configuration.GetSection(nameof(GrpcSettings))
+            .Get<GrpcSettings>();
         
         ArgumentNullException.ThrowIfNull(eventBusSettings);
         ArgumentNullException.ThrowIfNull(cacheSettings);
+        ArgumentNullException.ThrowIfNull(grpcSettings);
         
         services.AddSingleton(eventBusSettings);
         services.AddSingleton(cacheSettings);
+        services.AddSingleton(grpcSettings);
         
         return services;
     }
@@ -51,6 +58,20 @@ public static class ServiceExtensions
             options.Configuration = settings.ConnectionString;
         });
         services.AddDistributedMemoryCache();
+        return services;
+    }
+    
+    public static IServiceCollection ConfigureGrpcServices(this IServiceCollection services)
+    {
+        var settings = services.GetOptions<GrpcSettings>(nameof(GrpcSettings));
+        if(string.IsNullOrEmpty(settings.StockUrl))
+            throw new ArgumentNullException(
+                $"Grpc settings is not configured. {nameof(settings.StockUrl)}");
+        
+        services.AddGrpcClient<StockProtoService.StockProtoServiceClient>(x 
+            => x.Address = new Uri(settings.StockUrl));
+        services.AddScoped<StockItemGrpcService>();
+
         return services;
     }
 
---
+++ src/Services/Basket.API/GrpcServices/StockItemGrpcService.cs
@@ -0,0 +1,35 @@
+﻿using Inventory.Grpc.Protos;
+using ILogger = Serilog.ILogger;
+
+namespace Basket.API.GrpcServices;
+
+public class StockItemGrpcService
+{
+    private readonly StockProtoService.StockProtoServiceClient _stockProtoService;
+    private readonly ILogger _logger;
+    public StockItemGrpcService(StockProtoService.StockProtoServiceClient stockProtoService,
+        ILogger logger)
+    {
+        ArgumentNullException.ThrowIfNull(stockProtoService);
+        ArgumentNullException.ThrowIfNull(logger);
+
+        _stockProtoService = stockProtoService;
+        _logger = logger;
+    }
+
+    public async Task<StockModel> GetStock(string itemNo)
+    {
+        try
+        {
+            var stockItemRequest = new GetStockRequest { ItemNo = itemNo };
+            var result = await _stockProtoService.GetStockAsync(stockItemRequest);
+            _logger.Information("GetStock request for item no: {ItemNo}", itemNo);
+            return result;
+        }
+        catch (Exception ex)
+        {
+            _logger.Error(ex, "Error while getting stock for item no: {ItemNo}", itemNo);
+            return new StockModel(); 
+        }
+    }
+}
