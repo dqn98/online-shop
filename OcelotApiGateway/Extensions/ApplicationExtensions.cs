c47753a71b03b93b3a8aaf4ef08845cd21a08c14
Microservice-Shop <namqd98@gmail.com>
2025-01-12 14:56:31 +0700

    init ocelot api gateway project

---
+++ OcelotApiGateway/Extensions/ApplicationExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Ocelot.Middleware;
+
+namespace OcelotApiGateway.Extensions;
+
+public static class ApplicationExtensions
+{
+    public static void UseInfrastructure(this IApplicationBuilder app)
+    {
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapDefaultControllerRoute();
+        });
+            
+        app.UseCors("CorsPolicy");
+        // app.UseMiddleware<ErrorWrappingMiddleware>();
+        app.UseAuthorization();
+        app.UseOcelot().Wait();
+    }
+}
---
+++ OcelotApiGateway/Program.cs
@@ -0,0 +1,50 @@
+using OcelotApiGateway;
+using OcelotApiGateway.Extensions;
+using Scalar.AspNetCore;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+
+var builder = WebApplication.CreateBuilder(args);
+
+Log.Information($"Start {builder.Environment.ApplicationName} up");
+
+try
+{
+    builder.Configuration.AddAppConfigurations();
+    builder.Services.AddConfigurationSettings(builder.Configuration);
+    builder.Services.AddOpenApi();
+    builder.Services.AddControllers();
+    builder.Services.ConfigureOcelot(builder.Configuration);
+    builder.Services.ConfigureCors(builder.Configuration);
+    
+    var app = builder.Build();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.MapOpenApi();
+        app.MapScalarApiReference();
+    }
+
+    app.UseInfrastructure();
+
+    app.Run();
+
+}
+catch (Exception ex)
+{
+    string type = ex.GetType().Name;
+    if (type.Equals("StopTheHostException", StringComparison.Ordinal))
+    {
+        throw;
+    }
+
+    Log.Fatal(ex, $"Unhandled exception: {ex.Message}");
+}
+finally
+{
+    Log.Information($"Shut down {builder.Environment.ApplicationName} complete");
+    Log.CloseAndFlush();
+}
