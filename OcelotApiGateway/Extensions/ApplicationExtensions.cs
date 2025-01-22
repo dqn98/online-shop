1f2ef3e6ab40bd3e5fb3fdc388eed2b971c2814f
Microservice-Shop <namqd98@gmail.com>
2025-01-22 07:35:20 +0700

    add token controller for ocelot apigateway

--- OcelotApiGateway/Extensions/ApplicationExtensions.cs
+++ OcelotApiGateway/Extensions/ApplicationExtensions.cs
@@ -7,18 +7,21 @@ public static class ApplicationExtensions
 {
     public static void UseInfrastructure(this IApplicationBuilder app)
     {
+        app.UseAuthentication();
         app.UseRouting();
 
         app.UseAuthorization();
 
         app.UseEndpoints(endpoints =>
         {
-            endpoints.MapDefaultControllerRoute();
+            endpoints.MapGet("/", async context =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
         });
             
         app.UseCors("CorsPolicy");
         app.UseMiddleware<ErrorWrappingMiddleware>();
-        app.UseAuthorization();
         app.UseOcelot().Wait();
     }
 }
